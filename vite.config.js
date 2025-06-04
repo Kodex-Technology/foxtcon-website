309b350fff72b8a6e2cdb32aa528cde2455806a6
Jawad <jawad.a@kodextech.com>
2025-06-04 16:59:56 +0500

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react-swc"
+import { defineConfig, loadEnv } from "vite"
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "")
+  return {
+    plugins: [react(), tailwindcss(), TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),],
+    base: env.VITE_BASE_PATH,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+     server: {
+      host: '0.0.0.0',
+      port: 3060,
+      allowedHosts: ['foxtocn.kodextech.com']
+    }
+  }
+})
