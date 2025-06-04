694338ecd390a095021351a3b018b3643cb2de67
ZeeshanAhsan <zeeshan@qw.kodextech.com>
2025-06-04 19:39:32 +0500

    update lint format

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,27 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig, loadEnv } from "vite"
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig, loadEnv } from "vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "")
+  const env = loadEnv(mode, process.cwd(), "");
   return {
-    plugins: [react(), tailwindcss(), TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),],
+    plugins: [
+      react(),
+      tailwindcss(),
+      TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+    ],
     base: env.VITE_BASE_PATH,
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
       },
     },
-     server: {
-      host: '0.0.0.0',
+    server: {
+      host: "0.0.0.0",
       port: 3060,
-      allowedHosts: ['foxtocn.kodextech.com']
-    }
-  }
-})
+      allowedHosts: ["foxtocn.kodextech.com"],
+    },
+  };
+});
