0ae1cdafd45e7cfecf2399e439dd5c704582d1df
ZeeshanAhsan <zeeshan.a@kodextech.com>
2025-06-25 14:22:45 +0500

    chat feature updated

--- src/components/superadmin/Chat/ChatFooter/ChatFooter.tsx
+++ src/components/superadmin/Chat/ChatFooter/ChatFooter.tsx
@@ -6,76 +6,77 @@ import { ChatFooterProps } from "@/types/chat";
 import ImagePreview from "../ImagePreview/ImagePreview";
 
 const ChatFooter = ({
-    onUploadFile,
-    fileInputRef,
-    selectedFiles,
-    removeImagePreview,
-    showFilePreview,
-    onSendMessage,
-    newMessage,
-    setNewMessage,
+  onUploadFile,
+  fileInputRef,
+  selectedFiles,
+  removeImagePreview,
+  showFilePreview,
+  onSendMessage,
+  newMessage,
+  setNewMessage,
+  onKeyDown,
 }: ChatFooterProps) => {
-    const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
 
-    useEffect(() => {
-        if (textareaRef.current) {
-            if (newMessage === "") {
-                textareaRef.current.style.height = "40px";
-            } else {
-                textareaRef.current.style.height = "40px";
-                const scrollHeight = textareaRef.current.scrollHeight;
-                textareaRef.current.style.height = Math.min(scrollHeight, 100) + "px";
-            }
-        }
-    }, [newMessage]);
+  useEffect(() => {
+    if (textareaRef.current) {
+      if (newMessage === "") {
+        textareaRef.current.style.height = "40px";
+      } else {
+        textareaRef.current.style.height = "40px";
+        const scrollHeight = textareaRef.current.scrollHeight;
+        textareaRef.current.style.height = Math.min(scrollHeight, 100) + "px";
+      }
+    }
+  }, [newMessage]);
 
+  const onMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const value = e.target.value;
+    setNewMessage(value);
+  };
 
-    const onMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        const value = e.target.value;
-        setNewMessage(value);
-    };
+  const disable = newMessage.trim() === "" && selectedFiles.length === 0;
 
-    const disable = newMessage.trim() === "" && selectedFiles.length === 0;
-
-    return (
-        <div className="chat-footer-wrapper">
-            <div className="chat-input">
-                <textarea
-                    ref={textareaRef}
-                    placeholder="Write your message here..."
-                    value={newMessage}
-                    onChange={onMessageChange}
-                />
-            </div>
-            <div className="chat-actions">
-                <div className="upload-wrapper" style={{ cursor: "pointer" }}>
-                    <label htmlFor="file-upload">
-                        <UploadChatIcon />
-                    </label>
-                    <input
-                        type="file"
-                        id="file-upload"
-                        multiple
-                        onChange={onUploadFile}
-                        ref={fileInputRef}
-                    />
-                </div>
-                <button
-                    onClick={onSendMessage}
-                    className={disable ? "disable" : ""}
-                    disabled={disable}
-                >
-                    <SendIcon />
-                </button>
-            </div>
-            {showFilePreview && selectedFiles.length > 0 && (
-                <ImagePreview
-                    selectedFiles={selectedFiles}
-                    onRemoveFile={removeImagePreview}
-                />
-            )}
+  return (
+    <div className="chat-footer-wrapper">
+      <div className="chat-input">
+        <textarea
+          ref={textareaRef}
+          placeholder="Write your message here..."
+          value={newMessage}
+          onChange={onMessageChange}
+          onKeyDown={onKeyDown}
+        />
+      </div>
+      <div className="chat-actions">
+        <div className="upload-wrapper" style={{ cursor: "pointer" }}>
+          <label htmlFor="file-upload">
+            <UploadChatIcon />
+          </label>
+          <input
+            type="file"
+            id="file-upload"
+            multiple
+            onChange={onUploadFile}
+            ref={fileInputRef}
+          />
         </div>
-    );
+        <button
+          onClick={onSendMessage}
+          className={disable ? "disable" : ""}
+          disabled={disable}
+        >
+          <SendIcon />
+        </button>
+      </div>
+      {showFilePreview && selectedFiles.length > 0 && (
+        <ImagePreview
+          selectedFiles={selectedFiles}
+          onRemoveFile={removeImagePreview}
+        />
+      )}
+    </div>
+  );
 };
 
 export default ChatFooter;
