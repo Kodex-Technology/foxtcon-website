24cc483f2426384a2a041dbce367f1a6e91c42bd
Jamwasif <wasif.a@kodextech.com>
2025-06-25 11:07:49 +0500

    update text word break

--- src/components/superadmin/Chat/ChatFooter/ChatFooter.tsx
+++ src/components/superadmin/Chat/ChatFooter/ChatFooter.tsx
@@ -6,69 +6,70 @@ import { ChatFooterProps } from "@/types/chat";
 import ImagePreview from "../ImagePreview/ImagePreview";
 
 const ChatFooter = ({
-  onUploadFile,
-  fileInputRef,
-  selectedFiles,
-  removeImagePreview,
-  showFilePreview,
-  onSendMessage,
-  newMessage,
-  setNewMessage,
+    onUploadFile,
+    fileInputRef,
+    selectedFiles,
+    removeImagePreview,
+    showFilePreview,
+    onSendMessage,
+    newMessage,
+    setNewMessage,
 }: ChatFooterProps) => {
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
 
-  useEffect(() => {
-    if (textareaRef.current) {
-      textareaRef.current.style.height = "40px";
-    }
-  }, []);
+    useEffect(() => {
+        if (textareaRef.current) {
+            if (newMessage === "") {
+                textareaRef.current.style.height = "40px";
+            } else {
+                textareaRef.current.style.height = "40px";
+                const scrollHeight = textareaRef.current.scrollHeight;
+                textareaRef.current.style.height = Math.min(scrollHeight, 100) + "px";
+            }
+        }
+    }, [newMessage]);
 
-  const onMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const value = e.target.value;
-    setNewMessage(value);
 
-    if (textareaRef.current) {
-      textareaRef.current.style.height = "40px";
-      const scrollHeight = textareaRef.current.scrollHeight;
-      textareaRef.current.style.height = Math.min(scrollHeight, 100) + "px";
-    }
-  };
+    const onMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const value = e.target.value;
+        setNewMessage(value);
+    };
 
-  return (
-    <div className="chat-footer-wrapper">
-      <div className="chat-input">
-        <textarea
-          ref={textareaRef}
-          placeholder="Write your message here..."
-          value={newMessage}
-          onChange={onMessageChange}
-        />
-      </div>
-      <div className="chat-actions">
-        <div className="upload-wrapper" style={{ cursor: "pointer" }}>
-          <label htmlFor="file-upload">
-            <UploadChatIcon />
-          </label>
-          <input
-            type="file"
-            id="file-upload"
-            multiple
-            onChange={onUploadFile}
-            ref={fileInputRef}
-          />
+    return (
+        <div className="chat-footer-wrapper">
+            <div className="chat-input">
+                <textarea
+                    ref={textareaRef}
+                    placeholder="Write your message here..."
+                    value={newMessage}
+                    onChange={onMessageChange}
+                />
+            </div>
+            <div className="chat-actions">
+                <div className="upload-wrapper" style={{ cursor: "pointer" }}>
+                    <label htmlFor="file-upload">
+                        <UploadChatIcon />
+                    </label>
+                    <input
+                        type="file"
+                        id="file-upload"
+                        multiple
+                        onChange={onUploadFile}
+                        ref={fileInputRef}
+                    />
+                </div>
+                <button onClick={onSendMessage}>
+                    <SendIcon />
+                </button>
+            </div>
+            {showFilePreview && selectedFiles.length > 0 && (
+                <ImagePreview
+                    selectedFiles={selectedFiles}
+                    onRemoveFile={removeImagePreview}
+                />
+            )}
         </div>
-        <button onClick={onSendMessage}>
-          <SendIcon />
-        </button>
-      </div>
-      {showFilePreview && selectedFiles.length > 0 && (
-        <ImagePreview
-          selectedFiles={selectedFiles}
-          onRemoveFile={removeImagePreview}
-        />
-      )}
-    </div>
-  );
+    );
 };
 
 export default ChatFooter;
